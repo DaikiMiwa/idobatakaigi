272de5c90f5e88181378f01dfdf334e35b543165
Daiki Miwa <miwa.daiki.mllab.nit@gmail.com>
2023-02-07 16:56:42 +0900

    add submit button

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,19 +1,19 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Grid } from "@material-ui/core";
 import { Avatar } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/Styles'
 import { gravatarPath } from "../gravatar";
 import MessageField from "./MessageFieldComponent";
+import MessageSubmitButton from "./MessageSubmitButton"
 
-const useStyles = makeStyles( {
-    root:{
+const useStyles = makeStyles({
+    root: {
         margin: 26,
-        gridRow: 2 ,
+        gridRow: 2,
     },
 })
 
-const MessageInputField = ({ name })=>
-{
+const MessageInputField = ({ name }) => {
     const classes = useStyles();
     const avatarPath = gravatarPath(name);
 
@@ -23,13 +23,13 @@ const MessageInputField = ({ name })=>
         <div className={classes.root}>
             <Grid container>
                 <Grid item xs={1}>
-                    <Avatar src={avatarPath}/>
+                    <Avatar src={avatarPath} />
                 </Grid>
                 <Grid item xs={10}>
-                    <MessageField name={name} setText={setText} text={text}/>
+                    <MessageField name={name} setText={setText} text={text} />
                 </Grid>
                 <Grid item xs={1}>
-                    ボタン
+                    <MessageSubmitButton name={name} setText={setText} text={text} />
                 </Grid>
             </Grid>
         </div>
